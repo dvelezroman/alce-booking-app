a857436d5ffde1e94cba4f4a6a41fb16209d84da
greverom <greverom4@gmail.com>
2024-09-03 21:00:31 -0500

    booking modal

--- src/app/pages/meeting-booking/meeting-booking.component.ts
+++ src/app/pages/meeting-booking/meeting-booking.component.ts
@@ -1,32 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import {CommonModule, NgForOf} from "@angular/common";
+
 
 @Component({
   selector: 'app-meeting-booking',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    CommonModule
   ],
   templateUrl: './meeting-booking.component.html',
   styleUrl: './meeting-booking.component.scss'
 })
-export class MeetingBookingComponent implements OnInit {
+export class MeetingBookingComponent implements OnInit, AfterViewInit {
+
+
   selectedDate: string = '';
   selectedTimeSlot: string = '';
   timeSlots: {label: string, value: number}[] = [];
   today: string = '';
   maxDate: string = '';
 
+  formattedDate: string = '';
+  formattedTime: string = '';
+  
+  showModal = false;
+  showSuccessModal = false;
+
   constructor() {
     this.initializeTimeSlots();
+
+
   }
 
   ngOnInit() {
     this.today = this.getTodayDate();
     this.maxDate = this.getMaxDate();
     this.selectedDate = this.today; // Set the default selected date to today
+  }
+
+  ngAfterViewInit() {
+    const dateInput = document.querySelector('input[type="date"]') as HTMLInputElement;
+    if (dateInput) {
+      dateInput.addEventListener('click', () => {
+        dateInput.showPicker(); 
+      });
+    }
   }
 
   initializeTimeSlots() {
@@ -59,12 +80,33 @@ export class MeetingBookingComponent implements OnInit {
     return this.formatDate(maxDate);
   }
 
+  formatTime(timeSlot: string): string {
+    const hour = parseInt(timeSlot, 10);
+    const period = hour >= 12 ? 'PM' : 'AM';
+    const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
+    return `${formattedHour} ${period}`;
+  }
+
   bookMeeting() {
     if (this.selectedDate && this.selectedTimeSlot) {
-      console.log(`Meeting booked on ${this.selectedDate} at ${this.selectedTimeSlot}`);
+      const dateObject = new Date(this.selectedDate);
+      this.formattedDate = this.formatDate(dateObject);
+      this.formattedTime = this.formatTime(this.selectedTimeSlot);
+
+      console.log(`Meeting booked on ${this.formattedDate} at ${this.formattedTime}`);
+
+      this.showSuccessModal = true;
+
+      setTimeout(() => {
+        this.showSuccessModal = false;
+      }, 4000);
       // Add your booking logic here, e.g., send the selected date and time to your backend API.
     } else {
-      alert('Please select both a date and a time slot.');
+      this.showModal = true;
+
+      setTimeout(() => {
+        this.showModal = false;
+      }, 2000);
     }
   }
 }
