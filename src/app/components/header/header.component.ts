96efbf6ab0769eed548c64b2fc4fdce5b4f50e42
greverom <greverom4@gmail.com>
2024-09-10 22:31:50 -0500

    estilos modales notificacion login y log out

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { UsersService } from '../../services/users.service';
 import { CommonModule } from '@angular/common';
@@ -13,9 +13,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-  constructor (private usersService: UsersService, private router: Router){}
+  isLoggedIn: boolean = false;
+  showLogoutModal: boolean = false;
+
+  constructor (private usersService: UsersService, private router: Router){ }
+
+  ngOnInit(): void {
+    this.usersService.isLoggedIn$.subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
 
   toggleNavbar() {
     const navbarBurger = document.querySelector('.navbar-burger');
@@ -27,8 +36,34 @@ export class HeaderComponent {
     }
   }
 
-  onLogout() {
-    this.usersService.logout();  
-    this.router.navigate(['/login']);  
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const screenWidth = (event.target as Window).innerWidth;
+    const navbarMenu = document.getElementById('navbarBasic');
+    const navbarBurger = document.querySelector('.navbar-burger');
+
+    if (screenWidth < 768 && navbarMenu && navbarMenu.classList.contains('is-active')) {
+      navbarMenu.classList.remove('is-active');
+      if (navbarBurger) {
+        navbarBurger.classList.remove('is-active');
+      }
+    }
+  }
+
+
+  openLogoutModal() {
+    this.showLogoutModal = true;
+  }
+
+
+  closeLogoutModal() {
+    this.showLogoutModal = false;
+  }
+
+
+  onConfirmLogout() {
+    this.usersService.logout();
+    this.router.navigate(['/login']);
+    this.closeLogoutModal(); 
   }
 }
