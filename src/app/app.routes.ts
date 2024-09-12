b6931ab981b191743093a883922046a5e83d9a45
Dario Velez Roman <dvelezroman@gmail.com>
2024-09-12 08:03:53 -0500

    Move registration to register-complete component
    Created a new component `register-complete` with the complete registration form and updated routing accordingly. Removed the old registration component and adjusted related files to use the new component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {MeetingBookingComponent} from "./pages/meeting-booking/meeting-booking.component";
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+import { RegisterCompleteComponent } from './pages/register-complete/register-complete.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
@@ -9,7 +9,7 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterCompleteComponent },
   { path: 'booking', component: MeetingBookingComponent },
   { path: 'contact', component: ContactComponent},
   { path: '**', redirectTo: '/home' }
