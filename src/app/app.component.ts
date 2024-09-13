2816d106d5e906a1ec680b177b242a7518783fb6
Dario Velez Roman <dvelezroman@gmail.com>
2024-09-13 15:35:12 -0500

    Add global spinner component
    Introduce a global spinner component for better loading indication. Included necessary service and interceptor modifications to show and hide the spinner during HTTP requests. Integrated the spinner component into the app root for seamless usage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@
 import { HeaderComponent } from './components/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SpinnerComponent} from "./components/spinner/spinner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    SpinnerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/services/http.interceptor.ts
+++ src/app/services/http.interceptor.ts
@@ -1,21 +1,25 @@
 import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
 import {inject} from "@angular/core";
 import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import {catchError, finalize, throwError} from 'rxjs';
+import {SpinnerService} from "./spinner.service";
 
 export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
   const accessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : '';
   const router = inject(Router);
-  // Clone the request and add the authorization header
+  const spinner = inject(SpinnerService);
+  // Show spinner before the request starts
+  spinner.show();
+
   const authReq = req.clone({
     setHeaders: {
       Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json',
     }
   });
-
   // Pass the cloned request with the updated header to the next handler
   return next(authReq).pipe(
+    finalize(() => spinner.hide()),
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
         // Redirect to the login page if the status code is 401 (Unauthorized)
