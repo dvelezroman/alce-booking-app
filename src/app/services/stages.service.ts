72c8eeb3a4af3d5feba5fbfdbad060c5e76c6ec4
Dario Velez Roman <dvelezroman@gmail.com>
2024-09-16 15:48:22 -0500

    Add Stage Component and CRUD operations in Stages Service
    Created a new standalone StageComponent with its template and spec files. Introduced CreateStageDto and UpdateStageDto interfaces and added CRUD methods in the Stages Service to interact with stage entities.

--- src/app/services/stages.service.ts
+++ src/app/services/stages.service.ts
@@ -1,5 +1,5 @@
 import {Store} from "@ngrx/store";
-import {Stage} from "./dtos/student.dto";
+import {CreateStageDto, Stage, UpdateStageDto} from "./dtos/student.dto";
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
@@ -16,7 +16,23 @@ export class StagesService {
     private store: Store,
   ) {}
 
+  getById(id: number): Observable<Stage> {
+    return this.http.get<Stage>(`${this.apiUrl}/${id}`);
+  }
+
   getAll(): Observable<Stage[]> {
     return this.http.get<Stage[]>(`${this.apiUrl}`)
+  }
+
+  create(data: CreateStageDto): Observable<Stage> {
+    return this.http.post<Stage>(`${this.apiUrl}`, data);
+  }
+
+  update(id: number, data: UpdateStageDto): Observable<Stage> {
+    return this.http.patch<Stage>(`${this.apiUrl}/${id}`, data);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
