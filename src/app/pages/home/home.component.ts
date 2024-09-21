5923d17f02ea1b2e3d9726d91e24c5c8182b5984
greverom <greverom4@gmail.com>
2024-09-21 03:00:43 -0500

    estilos actualizados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectIsLoggedIn } from '../../store/user.selector';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+      CommonModule,
+      RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedIn: boolean = false;
 
+  constructor(private store: Store) {
+    this.isLoggedIn$ = this.store.select(selectIsLoggedIn);
+  }
+
+  ngOnInit() {
+    this.isLoggedIn$.subscribe(state => {
+      this.isLoggedIn = state;
+    });
+  }
 }
