e02f30f8c6b67aa806d180291fc3f0ce5f5b634e
greverom <greverom4@gmail.com>
2024-09-24 23:06:26 -0500

    componente searching-meeting estilos responsive

--- src/app/pages/searching-meeting/searching-meeting.component.ts
+++ src/app/pages/searching-meeting/searching-meeting.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BookingService } from '../../services/booking.service';
 import { NgModel } from '@angular/forms';
-import { MeetingDTO } from '../../services/dtos/booking.dto';
+import { MeetingDTO, UpdateMeetingLinkDto } from '../../services/dtos/booking.dto';
 
 @Component({
   selector: 'app-searching-meeting',
@@ -18,22 +18,35 @@ import { MeetingDTO } from '../../services/dtos/booking.dto';
   styleUrl: './searching-meeting.component.scss'
 })
 export class SearchingMeetingComponent implements OnInit {
- 
+  isModalOpen = false;
+  link: string = ''; 
+  availableHours: number[] = [];
+  meetings: MeetingDTO[] = [];
+  
   filter: { from: string, to: string, hour?: number } = {
     from: '',
-    to: ''
+    to: '',
+     hour: 9
   };
-
-  meetings: MeetingDTO[] = [];
-
   constructor(private bookingService: BookingService) {}
 
   ngOnInit(): void {
     const today = new Date();
     this.filter.from = today.toISOString().split('T')[0];  
+
+    this.availableHours = Array.from({ length: 13 }, (_, i) => 9 + i);
   }
 
-  onHourChange(): void {
+  openModal(): void {
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
+
+
+  onFilterChange(): void {
     const filterParams = {
       ...this.filter,
       hour: this.filter.hour?.toString() 
@@ -44,8 +57,24 @@ export class SearchingMeetingComponent implements OnInit {
     });
   }
 
-  onAssign(): void {
- 
+  assignLink(): void {
+    this.meetings.forEach(meeting => {
+      const updateData: UpdateMeetingLinkDto = {
+        date: new Date(meeting.date).toISOString(), 
+        hour: meeting.hour,
+        link: this.link  
+      };
+  
+      this.bookingService.updateMeetingLink(updateData).subscribe(
+        response => {
+          console.log('Link asignado correctamente', response);
+          this.closeModal();  
+        },
+        error => {
+          console.error('Error al asignar el link', error);
+        }
+      );
+    });
   }
 
 }
