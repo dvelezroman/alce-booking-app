5bdae7a40f0a975e05613ed7d348f83c228226e4
Dario Velez Roman <dvelezroman@gmail.com>
2024-10-01 08:14:10 -0500

    Add user search and cleanup on destroy in users.service
    Implemented a search function for filtering users by various criteria such as email, first name, last name, status, role, and registration status. Additionally, added logic in `ngOnDestroy` to complete a subject and clean up subscriptions to prevent memory leaks.

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,6 @@
 import {Injectable, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable, tap} from 'rxjs';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable, Subject, tap} from 'rxjs';
 import {environment} from "../../environments/environment";
 import {Store} from "@ngrx/store";
 import {setAdminStatus, setLoggedInStatus, setUserData, unsetUserData} from "../store/user.action";
@@ -15,6 +15,7 @@ export class UsersService implements OnInit{
   private apiUrl = `${environment.apiUrl}/users`;
   private userData$: Observable<UserDto | null>;
   userData: UserDto | null = null;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     private http: HttpClient,
@@ -29,6 +30,11 @@ export class UsersService implements OnInit{
       console.log(state);
       this.userData = state;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   register(user: Partial<UserDto>): Observable<RegisterResponseDto> {
@@ -92,4 +98,35 @@ export class UsersService implements OnInit{
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
+  searchUsers(
+    email?: string,
+    firstName?: string,
+    lastName?: string,
+    status?: string,
+    role?: string,
+    register?: boolean
+  ): Observable<UserDto[]> {
+    let params = new HttpParams();
+
+    if (email) {
+      params = params.set('email', email);
+    }
+    if (firstName) {
+      params = params.set('firstName', firstName);
+    }
+    if (lastName) {
+      params = params.set('lastName', lastName);
+    }
+    if (status) {
+      params = params.set('status', status);
+    }
+    if (role) {
+      params = params.set('role', role);
+    }
+    if (register) {
+      params = params.set('register', register.toString());
+    }
+
+    return this.http.get<UserDto[]>(`${this.apiUrl}/search`, { params });
+  }
 }
