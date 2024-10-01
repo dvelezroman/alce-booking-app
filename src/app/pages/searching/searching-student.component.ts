8909f309a077d8a7d84631b46be1bfc546fec2f0
greverom <greverom4@gmail.com>
2024-10-01 00:20:57 -0500

    validators y modal agregado searching

--- src/app/pages/searching/searching-student.component.ts
+++ src/app/pages/searching/searching-student.component.ts
@@ -1,19 +1,87 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-searching-student',
   standalone: true,
   imports: [
-      CommonModule
+      CommonModule,
+      ReactiveFormsModule,
+      ModalComponent
   ],
   templateUrl: './searching-student.component.html',
   styleUrl: './searching-student.component.scss'
 })
 export class SearchingStudentComponent {
   isStudentForm = true;
+  studentForm!: FormGroup;
+  userForm!: FormGroup; 
+
+  showModal: boolean = false; 
+  modalMessage: string = '';
+  modalIsError: boolean = false;
+  modalIsSuccess: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.studentForm = this.fb.group({
+      userId: [''], 
+      firstName: [''], 
+      lastName: [''] 
+    });
+
+    this.userForm = this.fb.group({
+      email: ['', Validators.required],  
+      role: ['', Validators.required]  
+    });
+  }
 
   toggleForm() {
-    this.isStudentForm = !this.isStudentForm;
+    if (this.isStudentForm) {
+      this.userForm.reset();  
+    } else {
+      this.studentForm.reset(); 
+    }
+    this.isStudentForm = !this.isStudentForm;  
   }
+
+  onSubmit() {
+    if (this.isStudentForm) {
+      const { userId, firstName, lastName } = this.studentForm.value;
+      if (userId) {
+        console.log('Buscar por ID Card:', userId);
+      } else if (firstName && lastName) {
+        console.log('Buscar por nombre y apellido:', firstName, lastName);
+      } else {
+        this.showErrorModal('Agregue el ID Card o los nombres del estudiante.');
+      }
+    } else {
+      if (this.userForm.valid) {
+        const { email, role } = this.userForm.value;
+        console.log('Buscar por email y rol:', email, role);
+      } else {
+        this.userForm.markAllAsTouched();
+        this.showErrorModal('Ambos campos son obligatorios.');
+      }
+    }
+  }
+
+  showErrorModal(message: string) {
+    this.modalIsSuccess = false;
+    this.modalIsError = true;
+    this.modalMessage = message;
+    this.showModal = true;
+
+    setTimeout(() => {
+      this.closeModal();
+    }, 3000); 
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
 }
