4c53a891cf8ec7a5d845e9802dc58c6911cdda7e
Gregorio Vélez Román <greverom4@gmail.com>
2024-11-20 10:15:36 -0500

    modal agregado

--- src/app/pages/register-student/register-student.component.ts
+++ src/app/pages/register-student/register-student.component.ts
@@ -3,29 +3,37 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { RegisterStudentDto } from '../../services/dtos/student.dto';
-import { UserDto } from '../../services/dtos/user.dto';
+import { UserDto, UserRole } from '../../services/dtos/user.dto';
 import { UsersService } from '../../services/users.service';
 import { StudentsService } from '../../services/students.service';
+import { ModalDto, modalInitializer } from '../../components/modal/modal.dto';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { Store } from '@ngrx/store';
+import { selectUserData } from '../../store/user.selector';
 
 @Component({
   selector: 'app-register-student',
   standalone: true,
   imports: [CommonModule,
             RouterModule,
-            ReactiveFormsModule],
+            ReactiveFormsModule,
+            ModalComponent],
   templateUrl: './register-student.component.html',
   styleUrl: './register-student.component.scss'
 })
 export class RegisterStudentComponent implements OnInit {
   registerForm: FormGroup;
   passwordVisible: boolean = false;
-  confirmPasswordVisible: boolean = false;  
+  confirmPasswordVisible: boolean = false; 
+  modal: ModalDto = modalInitializer(); 
+  user: UserDto | null = null;
 
   constructor(
     private fb: FormBuilder,
     private usersService: UsersService,
     private studentsService: StudentsService,
-    private router: Router
+    private router: Router,
+    private store: Store
   ) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -40,7 +48,11 @@ export class RegisterStudentComponent implements OnInit {
     }, { validator: this.passwordMatchValidator });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.store.select(selectUserData).subscribe((userData: UserDto | null) => {
+      this.user = userData;
+    });
+  }
 
   passwordMatchValidator(group: FormGroup) {
     const password = group.get('password')?.value;
@@ -67,35 +79,62 @@ export class RegisterStudentComponent implements OnInit {
   toggleConfirmPasswordVisibility(): void {
     this.confirmPasswordVisible = !this.confirmPasswordVisible;
   }
-
+  
   onSubmit() {
     if (this.registerForm.invalid) {
       this.markFormGroupTouched(this.registerForm);
+      this.showModal(this.createModalParams(true, 'El formulario debe ser completado.'));
       return;
     }
-
-    const userData: Partial<UserDto> = {
+  
+    const userData: UserDto = {
+      id: 0, 
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
       firstName: this.registerForm.value.firstName,
       lastName: this.registerForm.value.lastName,
       idNumber: this.registerForm.value.idNumber,
       birthday: new Date(this.registerForm.value.birthday).toISOString().split('T')[0],
-      email: this.registerForm.value.email,
-      password: this.registerForm.value.password,
-      role: this.registerForm.value.role
+      role: UserRole.STUDENT, 
     };
-
+  
     const studentData: RegisterStudentDto = {
-      userId: 0, 
-      mode: this.registerForm.value.mode
+      userId: null, 
+      mode: this.registerForm.value.mode,
+      stageId: undefined, 
     };
-
+  
     this.usersService.register(userData).subscribe({
-      next: (response: any) => {
-        console.log('User ID:', response.user.id); 
-        studentData.userId = response.user.id;
+      next: (userResponse) => {
+        studentData.userId = userResponse.user.id;
+  
+        this.studentsService.registerStudent(studentData).subscribe({
+          next: () => {
+            this.showModal(this.createModalParams(false, 'Registro exitoso.'));
+            this.router.navigate(['/home']);
+          },
+          error: (error) => {
+            console.error('Error al registrar estudiante:', error);
+            this.showModal(this.createModalParams(true, 'No se pudo registrar el estudiante.'));
+          },
+        });
       },
-      error: (error) => console.error('Error al registrar usuario:', error)
+      error: (error) => {
+        console.error('Error al registrar usuario:', error);
+        this.showModal(this.createModalParams(true, 'No se pudo registrar el usuario.'));
+      },
     });
+  }
+  
+  createModalParams(isError: boolean, message: string): ModalDto {
+    return {
+      ...this.modal,
+      show: true,
+      isError,
+      isSuccess: !isError,
+      message,
+      close: this.closeModal
+    };
   }
 
   private markFormGroupTouched(formGroup: FormGroup) {
@@ -105,4 +144,15 @@ export class RegisterStudentComponent implements OnInit {
       control?.markAsDirty();
     });
   }
+
+  showModal(params: ModalDto) {
+    this.modal = { ...params };
+    setTimeout(() => {
+      this.modal.close();
+    }, 2500); 
+  }
+  
+  closeModal = () => {
+    this.modal = { ...modalInitializer() };
+  };
 }
