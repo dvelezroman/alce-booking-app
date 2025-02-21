8d9d89bfdb16137ea4c9f385434342a3907c01f4
Gregorio Vélez Román <greverom4@gmail.com>
2025-02-21 02:16:48 -0500

    componente conctact innecesario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { MeetingBookingComponent } from "./pages/meeting-booking/meeting-booking
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterCompleteComponent } from './pages/register-complete/register-complete.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { RegisterComponent } from "./pages/register/register.component";
 import { StageComponent } from "./pages/stage/stage.component";
 import { SearchingMeetingComponent } from './pages/searching-meeting/searching-meeting.component';
@@ -27,7 +26,6 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'register-complete', component: RegisterCompleteComponent, canActivate: [AuthGuard] },
   { path: 'booking', component: MeetingBookingComponent, canActivate: [AuthGuard] },
-  { path: 'contact', component: ContactComponent, canActivate: [AuthGuard]},
   { path: 'stage', component: StageComponent, canActivate: [AuthGuard] },
   { path: 'link', component: LinksComponent},
   { path: 'searching-meeting', component: SearchingMeetingComponent, canActivate: [AuthGuard]},
