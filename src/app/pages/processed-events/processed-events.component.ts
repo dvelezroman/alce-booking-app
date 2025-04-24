1a6ff6108e5ef2aba260144aa62800e022ff8350
Gregorio Vélez Román <greverom4@gmail.com>
2025-04-23 22:36:45 -0500

    limpiar evento input cuando selecciona otro usuario

--- src/app/pages/processed-events/processed-events.component.ts
+++ src/app/pages/processed-events/processed-events.component.ts
@@ -81,6 +81,7 @@ export class ProcessedEventsComponent implements OnInit {
     this.filter.processedById = user.id;
     this.filteredUsers = [];
     this.showUserDropdown = false;
+    this.filter.eventType = undefined;
   }
 
   formatDate(date: string): string {
