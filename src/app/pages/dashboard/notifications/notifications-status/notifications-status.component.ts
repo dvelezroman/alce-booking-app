00facca00d08ee3e8a5915f2a4febb56abe335d4
Gregorio Vélez Román <greverom4@gmail.com>
2025-08-30 16:59:29 -0500

    feat: estilos responsivos y tabla unificada para vista de notificaciones

--- src/app/pages/dashboard/notifications/notifications-status/notifications-status.component.ts
+++ src/app/pages/dashboard/notifications/notifications-status/notifications-status.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NotificationService } from '../../../../services/notification.service';
-import {
-  FilterNotificationDto,
-  Notification,
-} from '../../../../services/dtos/notification.dto';
+import { FilterNotificationDto, Notification } from '../../../../services/dtos/notification.dto';
 
 @Component({
   selector: 'app-notifications-status',
@@ -17,20 +14,40 @@ import {
 export class NotificationsStatusComponent implements OnInit {
   notifications: Notification[] = [];
   statusFilter: 'SENT' | 'PENDING' | 'DELIVERED' | 'READ' | 'FAILED' | '' = '';
+  typeFilter: Notification['notificationType'] | '' = '';
+  scopeFilter: Notification['scope'] | '' = '';
   loading = false;
+
+  fromDate: string = '';
+  toDate: string = '';
 
   constructor(private notificationService: NotificationService) {}
 
   ngOnInit(): void {
+    const today = new Date().toISOString().slice(0, 10);
+    this.fromDate = today;
+    this.toDate = today;
     this.fetchNotifications();
   }
 
   fetchNotifications(): void {
     this.loading = true;
-    const filters: FilterNotificationDto = {};
+
+    const filters: FilterNotificationDto = {
+      fromDate: this.fromDate,
+      toDate: this.toDate,
+    };
 
     if (this.statusFilter) {
       filters.status = this.statusFilter;
+    }
+
+    if (this.scopeFilter) {
+      filters.scope = this.scopeFilter;
+    }
+
+    if (this.typeFilter) {
+      filters.notificationType = this.typeFilter;
     }
 
     this.notificationService.getNotifications(filters).subscribe({
