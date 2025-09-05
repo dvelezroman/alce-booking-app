2e9f2b52ae71c9ad68285d686e1bc5e79528e8f2
Gregorio Vélez Román <greverom4@gmail.com>
2025-09-05 14:57:40 -0500

    feat(whatsapp): creamos página de envíos por whatsApp y maquetada page ui

---
+++ src/app/components/whatsapp/schedule-bar/schedule-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-schedule-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './schedule-bar.component.html',
+  styleUrls: ['./schedule-bar.component.scss'],
+})
+export class ScheduleBarComponent {}
---
+++ src/app/components/whatsapp/summary-footer/summary-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-summary-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './summary-footer.component.html',
+  styleUrls: ['./summary-footer.component.scss'],
+})
+export class SummaryFooterComponent {}
