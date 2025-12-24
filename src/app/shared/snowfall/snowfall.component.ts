3cf645f43d3f5496e450956c1bb9cd5378eb3ab6
Gregorio Vélez Román <greverom4@gmail.com>
2025-12-24 08:22:32 -0500

    feat: snow fall layout

---
+++ src/app/shared/snowfall/snowfall.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snowfall',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './snowfall.component.html',
+  styleUrl: './snowfall.component.scss'
+})
+
+export class SnowfallComponent implements OnInit {
+  flakes = Array.from({ length: 200 });
+
+  ngOnInit() {
+    setTimeout(() => this.randomizeFlakes(), 0);
+  }
+
+  randomizeFlakes() {
+    const flakes = document.querySelectorAll('.snowflake');
+
+    flakes.forEach((flake: any) => {
+      const size = Math.random() * 4 + 2;
+      const duration = Math.random() * 10 + 8;
+      const delay = Math.random() * 10;
+      const left = Math.random() * 100;
+
+      flake.style.width = `${size}px`;
+      flake.style.height = `${size}px`;
+      flake.style.left = `${left}vw`;
+      flake.style.animationDuration = `${duration}s`;
+      flake.style.animationDelay = `${delay}s`;
+      flake.style.opacity = Math.random() * 0.6 + 0.3;
+    });
+  }
+}
