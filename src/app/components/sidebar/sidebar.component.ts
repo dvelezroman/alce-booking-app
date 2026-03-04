0ac0dfda0aaa78b18a352734ffcc2b2d79140812
Gregorio Vélez Román <greverom4@gmail.com>
2026-03-04 08:39:35 -0500

    fix(assessments): ajustamos nombre de la página y corregimos visualización de fechas

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -91,7 +91,7 @@ navItems: SidebarNavItem[] = [
   { icon: 'test', text: 'Clases Evaluadas', route: '/dashboard/meeting-evaluations', roles: [UserRole.ADMIN],  },
   { icon: 'test', text: 'Estadísticas', route: '/dashboard/evaluation-statistics', roles: [UserRole.ADMIN],  },
   { icon: 'test', text: 'Evaluaciones', route: '/dashboard/assessment-reports', roles: [UserRole.ADMIN] },
-  { icon: 'assessment', text: 'Assessments Pendientes', route: '/dashboard/stage-assessment-student', roles: [UserRole.STUDENT] },
+  { icon: 'assessment', text: 'Assessments', route: '/dashboard/stage-assessment-student', roles: [UserRole.STUDENT] },
   { icon: 'book-open', text: 'Recursos Académicos', route: '/dashboard/resources', roles: [UserRole.ADMIN]},
   { icon: 'book-open', text: 'Recursos Stages', route: '/dashboard/stage-assessment-resources', roles: [UserRole.ADMIN]},
   { icon: 'book-type', text: 'Tipos de Evaluación',  route: '/dashboard/assessment-types', roles: [UserRole.ADMIN]},
