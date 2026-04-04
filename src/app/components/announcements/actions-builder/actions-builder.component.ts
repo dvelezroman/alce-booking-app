298ea0a5e8945dd577ddb851b859a68759bd1c1a
Gregorio Vélez Román <greverom4@gmail.com>
2026-04-04 13:17:44 -0500

    feat(admin-announcements): mejora responsive en página de anuncios

--- src/app/components/announcements/actions-builder/actions-builder.component.ts
+++ src/app/components/announcements/actions-builder/actions-builder.component.ts
@@ -22,7 +22,7 @@ export interface ActionButton {
 })
 export class ActionsBuilderComponent {
 
-  delayOptions = [10, 20, 30, 40, 50, 60];
+  delayOptions = [10, 15, 20, 30, 40, 50, 60];
 
   @Input() actions: ActionButton[] = [];
 
